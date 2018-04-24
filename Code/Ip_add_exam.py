9cd5c18b4c88b558632ae86d5a8299002d7e27d2
ZhaoheZhang <zzh9414@gmail.com>
2018-04-24 16:14:07 -0500

    RF code that I will change later

--- Code/Ip_add_exam.py
+++ Code/Ip_add_exam.py
@@ -14,26 +14,43 @@ def load_data():
     training_data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
 
     # Extract the inputs from the training data array (all columns but the last one)
-    inputs = training_data[:, 0]
+    #inputs = training_data[:, 0]
 
     # Extract the outputs from the training data array (last column)
     outputs = training_data[:, -1]
 
     # Separate the training and testing data
-    training_inputs = inputs[:10000]
-    training_outputs = outputs[:10000]
-    testing_inputs = inputs[10000:]
-    testing_outputs = outputs[10000:]
+    #training_inputs = inputs[:10000]
+    training_outputs = outputs[:]
+    #testing_inputs = inputs[10000:]
+    #testing_outputs = outputs[:]
 
     # Return the four arrays
-    return training_inputs, training_outputs, testing_inputs, testing_outputs
-
+    #return training_inputs, training_outputs, testing_inputs, testing_outputs
+    return training_outputs
 
 if __name__ == '__main__':
-    print "Find out corrlation between IP length and phishing site"
 
     # Load the training data
-    train_inputs, train_outputs, test_inputs, test_outputs = load_data()
+    test_outputs = load_data()
+
+    counter = 0
+    counter2 = 0
+    for i in range(len(test_outputs)):
+        if test_outputs[i] == 1:
+            counter+=1
+        else:
+            counter2+=1
+
+    print "Phishing #: " + str(counter)
+    print "Non-Phishing #: " + str(counter2)
+
+    total = counter + counter2
+
+    print "total: " + str(total)
+    """
+    print "Find out corrlation between IP length and phishing site"
+
 
     print "Training data loaded."
 
@@ -52,3 +69,7 @@ if __name__ == '__main__':
     # Print the accuracy (percentage of phishing websites correctly predicted)
     accuracy = 100.0 * accuracy_score(test_outputs, predictions)
     print "The accuracy of your decision tree on testing data is: " + str(accuracy)
+
+    
+    
+    """
--- Code/RandomForest.py
+++ Code/RandomForest.py
@@ -1,9 +1,32 @@
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 import numpy as np
 
-from decision_tree import load_data
+
+def load_data():
+
+    # Load the training data from the CSV file
+    training_data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
+
+    # Extract the inputs from the training data array (all columns but the last one)
+    inputs = training_data[:, [0,1,3,8,14,16,18,22,23]]
+
+    # Extract the outputs from the training data array (last column)
+    outputs = training_data[:, -1]
+
+    # Separate the training and testing data
+    training_inputs = inputs[:10000]
+    training_outputs = outputs[:10000]
+    testing_inputs = inputs[10000:]
+    testing_outputs = outputs[10000:]
+
+    # Return the four arrays
+    return training_inputs, training_outputs, testing_inputs, testing_outputs
+
 
 
 if __name__ == '__main__':
@@ -29,6 +52,15 @@ if __name__ == '__main__':
     # Print the accuracy (percentage of phishing websites correctly predicted)
     accuracy = 100.0 * accuracy_score(test_outputs, predictions)
     print "The accuracy of Random Forest on testing data is: " + str(accuracy)
+
+    f1 = f1_score(test_outputs, predictions, average='micro')
+    print "The F1 score of Random Forest is: " + str(f1)
+
+    pre = precision_score(test_outputs, predictions, average='micro')
+    print "The precision for Random Forest on testing data is: " + str(pre)
+
+    recall = recall_score(test_outputs, predictions, average='micro')
+    print "The recall value for Random Forest on testing data is: " + str(recall)
 
 
     #1. training size and testing accurcy
