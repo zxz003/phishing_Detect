27c6028cc34cf341dfabe2fe4d55328c786f4253
ZhaoheZhang <zzh9414@gmail.com>
2018-05-05 00:50:04 -0500

    Update the new RoC

--- Code/Testing_Code/testROC.py
+++ Code/Testing_Code/testROC.py
@@ -15,10 +15,11 @@ X = iris.data
 y = iris.target
 
 # Binarize the output
-y = label_binarize(y, classes=[0, 1, 2])
-n_classes = y.shape[1]                  # y.shape = (1,3) and y.shape[1] = 3 which is class size
+y = label_binarize(y, classes=[0,1,2])
+n_classes = y.shape[1]                       # y.shape = (1,3) and y.shape[1] = 3 which is class size
 
 # Add noisy features to make the problem harder
+
 random_state = np.random.RandomState(0)
 n_samples, n_features = X.shape
 X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
@@ -29,27 +30,38 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5,
 
 # Learn to predict each class against the other
 classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
-                                 random_state=random_state))
+                                   random_state=random_state))
 y_score = classifier.fit(X_train, y_train).decision_function(X_test)
 
 # Compute ROC curve and ROC area for each class
 fpr = dict()
 tpr = dict()
 roc_auc = dict()
+
 for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    fpr[i], tpr[i], thresholds = roc_curve(y_test[:, i], y_score[:, i])
     roc_auc[i] = auc(fpr[i], tpr[i])
 
+#print ('y_test: \n{}'.format(y_test))
+print ('y_score: \n{}'.format(y_score))
+print ('thresholds: \n{}'.format(thresholds))
+'''
+fpr, tpr, _ = roc_curve(y_test, y_score)
+roc_auc= auc(fpr, tpr)
+
+'''
 # Compute micro-average ROC curve and ROC area
+print ('y_testRavel: \n{}'.format(y_test.ravel()))
 fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+
 roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
 
 #Plot the graph
 plt.figure()
 lw = 3                      # Line width
-plt.plot(fpr[1], tpr[1], color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot(fpr[0], tpr[0], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
 plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
